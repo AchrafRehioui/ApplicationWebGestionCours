fd9e24409f45ae81742917bab4ebcdb51f89f7f8
HDA <achraf.rehioui.1@gmail.com>
2020-10-07 01:24:34 +0200

    Ajout de la suppression de cours

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const list = document.querySelector('ul');
 const form = document.querySelector('form');
 
+list.addEventListener('click', e => {
+    if(e.target.tagName === "BUTTON"){
+        let id = e.target.parentElement.getAttribute('data-id');
+
+        db.collection("cours").doc(id).delete()
+            .then(() => console.log('supprimé'))
+            .catch(err => console.error(err))
+    }
+})
+
 form.addEventListener('submit', e => {
+    
     //sousmission du formulaire
     e.preventDefault();
-     
+
     const now = new Date();
+
     const cours = {
         title: form.cours.value,
         created_at: firebase.firestore.Timestamp.fromDate(now)
@@ -17,19 +29,22 @@ form.addEventListener('submit', e => {
 
 })
 
-AjoutCours = cours => {
+AjoutCours = (cours, id) => {
+
     const html = `
-        <li class="list-group-item">
+        <li class="list-group-item" data-id="{${id}}">
             <h3>${cours.title}</h3>
             <small>${cours.created_at.toDate()}</small>
+            <button class ="btn btn-danger btn-sm my-3">Supprimer</button>
         </li>    
-    `
+       `
     list.innerHTML += html;
 }
 
 db.collection("cours").get()
     .then(res => res.docs.forEach(cours => {
-        AjoutCours(cours.data())
+        console.log(cours.id)
+        AjoutCours(cours.data(), cours.id)
     }))
     .catch(err => console.error(err))
 
